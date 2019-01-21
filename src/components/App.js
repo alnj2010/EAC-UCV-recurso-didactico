dcec9d13e70a7f32d83e4e4a5dfea712f4667c01
Abraham Navarro <anavarro@novopayment.com>
2019-01-21 11:51:00 -0400

    Reestructura las carpetas.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './global/images/logo.svg';
+import './global/css/App.css';
 
 class App extends Component {
   render() {
