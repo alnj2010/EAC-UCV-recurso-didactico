06382318f1a16295db24d2ee8ef4b5ff82f855d1
alnj2010 <abrahancho6@hotmail.com>
2019-01-24 13:51:51 -0400

    Incorpora boton go back.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,13 +2,18 @@
 import React from 'react'
 //Assets
 import './Navbar.css'
+import backicon from '../global/images/back.svg'
 
 function Navbar(props){
     const {logo, children} = props;
-
+    var back=window.location.pathname==='/'?'':<div onClick={()=>{window.history.back();}}> <img src={backicon} alt=""/> </div>;
     return (
         <nav>
-            <div className="logo"> <img src={logo} alt="logo"/> </div>
+            
+            <div className="logo">
+                {back}
+                <img src={logo} alt="logo"/> 
+            </div>
             <ul className="menu">
                 {children.map((item,index)=> ( <li key={index.toString()}> {item} </li>))}
             </ul>
