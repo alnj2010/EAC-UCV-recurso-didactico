8818efd76f4e4ec13e43cde0a04cc09945aa0b5c
alnj2010 <abrahancho6@hotmail.com>
2019-01-25 01:16:07 -0400

    Incorpora contenido en la seccion blockchain.

--- src/components/Blockchain/Blockchain.js
+++ src/components/Blockchain/Blockchain.js
@@ -19,7 +19,41 @@ function Blockchain(){
             <Presentation title="Lorem Ipsum">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis veritatis repellendus ab excepturi amet, vero autem. Sed facilis aliquam ab quaerat maxime ea iste voluptatum inventore, omnis, alias magni? Sit?
             </Presentation>
-
+            <div className="content">
+                <article>
+                    <h1> Lorem ipsum </h1>
+                    <p>
+                        <div> <img src="" alt=""/></div>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, expedita itaque! Rem, animi harum consequatur
+                        facilis nihil, modi facere, adipisci nam asperiores nisi illo atque! Officiis modi temporibus deserunt voluptatem.
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, expedita itaque! Rem, 
+                        animi harum consequatur facilis nihil, modi facere, adipisci nam asperiores nisi 
+                        illo atque! Officiis modi temporibus deserunt voluptatem.Lorem ipsum, dolor sit
+                         amet consectetur adipisicing elit. Quam, expedita itaque! Rem, animi harum 
+                         consequatur facilis nihil, modi facere, adipisci nam asperiores nisi illo atque! 
+                        Officiis modi temporibus deserunt voluptatem.Lorem ipsum, dolor sit amet 
+                        consectetur adipisicing elit. Quam, expedita itaque! Rem, animi harum consequatur 
+                        facilis nihil, modi facere, adipisci nam asperiores nisi illo atque! Officiis
+                        modi temporibus deserunt voluptatem.Lorem ipsum, dolor sit amet consectetur
+                        adipisicing elit. Quam, expedita itaque! Rem, animi harum consequatur facilis
+                        nihil, modi facere, adipisci nam asperiores nisi illo atque! Officiis modi 
+                        temporibus deserunt voluptatem.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, 
+                        expedita itaque! Rem, animi harum consequatur facilis nihil, modi facere, adipisci 
+                        nam asperiores nisi illo atque! Officiis modi temporibus deserunt voluptatem.Lorem ipsum, dolor sit amet consectetur 
+                        adipisicing elit. Quam, expedita itaque! Rem, animi harum consequatur facilis nihil, modi facere, adipisci nam 
+                        asperiores nisi illo atque! Officiis modi temporibus deserunt voluptatem.
+                    </p>
+                </article>
+                <article>
+                    <h1>Lorem ipsum</h1>
+                    <div className="video"><iframe width="854" height="510" src="https://www.youtube.com/embed/Bo8815AeU7E" title="blockchain video" frameborder="0"></iframe></div>
+                    
+                    <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum perferendis excepturi, deleniti exercitationem sit minima eos, ut placeat, et ullam saepe explicabo magnam dolores. Nulla eos dolorum sed iusto enim.Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                        </p>
+                        <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum perferendis excepturi, deleniti exercitationem sit minima eos, ut placeat, et ullam saepe explicabo magnam dolores. Nulla eos dolorum sed iusto enim.Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                        </p>
+                </article>
+            </div>
             <Knowthat>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis quis itaque eos non accusantium, voluptate dolore quam delectus officiis voluptatibus assumenda cum, placeat sint quod, deleniti ducimus sed aperiam veritatis.</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis quis itaque eos non accusantium, voluptate dolore quam delectus officiis voluptatibus assumenda cum, placeat sint quod, deleniti ducimus sed aperiam veritatis.</p>
